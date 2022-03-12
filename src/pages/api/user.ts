f34c6e8661f7439e48ac21d969f47215c6e88b52
Geunta Buwono <25718928+GeuntaBuwono@users.noreply.github.com>
2022-03-13 04:09:24 +0700

    feat: integrate to API

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -1,14 +1,15 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { users } from '../../mock/users';
 
-// TODO integrate with this endpoint
-// https://randomuser.me/api/?results=10&page=1&inc=id,picture,name,phone,dob,email&seed=foobar&name=betty
-export default function UserHandler(req: NextApiRequest, res: NextApiResponse) {
+export default async function UserHandler(req: NextApiRequest, res: NextApiResponse) {
   if (req.query) {
-    res.status(200).json(users);
+    const reqData = await fetch(
+      `https://randomuser.me/api/?results=5&inc=id,picture,name,phone,dob,email&seed=foobar&page=${req.query.page}`
+    );
+    const data = await reqData.json();
+    res.status(200).json(data);
   } else {
     res.status(404).json({
-      message: 'need query params on query string'
+      message: 'need page on query string'
     });
   }
 }
