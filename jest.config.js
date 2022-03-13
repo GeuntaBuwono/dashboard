68435f6072db81d5b947cfd057572e105d0be2ab
Geunta Buwono <25718928+GeuntaBuwono@users.noreply.github.com>
2022-03-13 19:15:36 +0700

    chore: remove cypress

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,12 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+
 module.exports = {
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/', '<rootDir>/cypress/'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   setupFilesAfterEnv: ['<rootDir>/setupTest.ts'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  moduleNameMapper: {
-    '^@components(.*)$': '<rootDir>/src/components$1',
-    '^@lib(.*)$': '<rootDir>/src/lib$1',
-    '^@pages(.*)$': '<rootDir>/src/pages$1'
-  }
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths)
 };
