26ebd17709228f9bf2b952c3627cc0aaee93ed2e
Geunta Buwono <25718928+GeuntaBuwono@users.noreply.github.com>
2022-03-15 01:37:14 +0700

    feat: hooks useLocalStorage

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+
+interface UseGetStorageProps {
+  key: 'users';
+  defaultValue: string;
+}
+
+const getStorageValue = ({ key, defaultValue }: UseGetStorageProps) => {
+  if (typeof window !== 'undefined') {
+    const saved = localStorage.getItem(key);
+
+    if (typeof saved !== undefined) {
+      const initial = saved !== null ? JSON.parse(saved) : defaultValue;
+      return initial;
+    }
+  }
+};
+
+const useLocalStorage = ({ key, defaultValue }: UseGetStorageProps) => {
+  const [value, setValue] = useState(() => {
+    return getStorageValue({
+      key,
+      defaultValue
+    });
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+};
+
+export default useLocalStorage;
